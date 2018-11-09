83756469f631c731a411f98101356a5405d68688
Wang <WANGXIAO@mcpsmd.org>
2018-11-09 09:40:10 -0500

    Initial commit

---
+++ test1.py
@@ -0,0 +1,30 @@
+print(5+5)
+
+print('hello world')
+test = 'abc'
+test2 = 'efg'
+print(test + test2)
+print("he said I'll go to bed soon")
+
+#user defined function
+
+def printfunction():
+    print("my first defined function!!!!")
+
+printfunction()
+
+
+num1 = 10
+num2 = '1'
+print(num1 + int(num2))
+
+print(len('test'))
+
+print(float(num1))
+
+
+testString = "my name is Sheldon"
+
+print(testString[11:18])
+
+
---
+++ test2.py
@@ -0,0 +1,14 @@
+import turtle
+
+
+my_turtle = turtle.Turtle()
+my_turtle.shape("turtle")
+my_turtle.forward(400)
+
+my_turtle.right(90)
+my_turtle.forward(200)
+my_turtle.right(90)
+my_turtle.forward(400)
+my_turtle.right(90)
+my_turtle.forward(200)
+turtle.mainloop()
